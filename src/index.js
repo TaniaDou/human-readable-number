2107ca7474ece72f8a6aba9bbcf27c3c29301a89
TaniaDou <t.doump@gmail.com>
2023-09-14 16:24:01 +0300

    fix ver 2

--- src/index.js
+++ src/index.js
@@ -3,37 +3,27 @@ module.exports = function toReadable (number) {
     let tens = ['hundred', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
     
     let decade = Math.floor(number / 10);
-    let hundreds = Math.floor(number / 100);
+    let k1 = Math.floor(number / 100);
+    let k2 = Math.floor(number % 100 / 10);
+    let k3 = Math.floor(number % 10);
+
 // от 0 до 19
-if (number >=0 && number <20) {
-    return teens[number]
-    };
+if (number >=0 && number <20) {return teens[number];}
 
  // от 20 до 99
 if (number >= 20 && number < 100) {
-    if (number % 10 != 0) {
-        return `${tens[decade]} ${teens[number % 10]}`};
-    }
-    else {
-        return tens[decade];
-    };
+    if (number % 10 !== 0) {return `${tens[decade]} ${teens[number % 10]}`;}}
+    if (number % 10 == 0) {return tens[decade];}
 
 // от 100 до 999
 if (number > 99 && number < 1000) {
-    let k1 = Math.floor(number / 100); //сколько сотен в числе
-    let k2 = Math.floor(number % 100 / 10);
-    let k3 = Math.floor(number % 10); //сколько единиц в числе
+    if (k2 == 0 && k3 == 0) {return `${teens[k1]} ${tens[0]}`}
+    if (k2 == 1 && k3 != 0) {return `${teens[k1]} ${tens[0]} ${teens[[k3 + 10]]}`}
+    if (k2 > 1 && k3 != 0)  {return `${teens[k1]} ${tens[0]} ${tens[k2]} ${teens[k3]}`}
+    if (k2 == 1 && k3 == 0)  {return `${teens[k1]} ${tens[0]} ${tens[k2]}`}
 
-    if (k2 != 0 && k3 != 0) {
-        if (k2 == 1) {
-            return `${teens[k1]} ${tens[0]} ${teens[[k3 + 10]]}`
-        }
-        else {
-            return `${teens[k1]} ${tens[0]} ${teens[k2]} ${teens[k3]}`;
-        };
-    };
-    if (k2 == 0 && k3 != 0) { return `${teens[k1]} ${tens[0]} ${teens[k3]}` };
-    if (k2 != 0 && k3 == 0) { return `${teens[k1]} ${tens[0]} ${tens[k2]}` };
-    if (k2 == 0 && k3 == 0) { return `${teens[k1]} ${tens[0]}` };
 }
 }
+
+
+
